e88f9fc2441b1156389d62c77eeaa0ffb657120b
chukwuebuka buka4chocksy <buka4chocksy@gmail.com>
2018-11-15 23:33:10 +0100

    added user model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+var userSchema = mongoose.Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  },
+  status: { type: String, required: true },
+  verify: { type: Boolean },
+  statusCode: { type: Number },
+  password: { type: String, required: true }
+});
+
+module.exports = mongoose.model("user", userSchema);
