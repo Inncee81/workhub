e90e205a3b1ec97ac96b715dcbc1879ac67b3483
buka4chocksy <buka4chocksy@gmail.com>
2018-12-03 15:37:12 +0100

    added update url and delete url in cv routes

--- routes/cvs.js
+++ routes/cvs.js
@@ -4,5 +4,7 @@ var router = express.Router();
 var cvController = require('../controllers/cvControllers');
 router.post('/create', cvController.createCv);
 router.post('/search',cvController.searchCv);
+router.get('/update/:id', cvController.updateCv);
+router.get('/delete/:id',cvController.deleteCV);
 
 module.exports = router;
