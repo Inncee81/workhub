fd0f267def0ae3b0eaf05aa6ebd3922f6bab624a
buka4chocksy <buka4chocksy@gmail.com>
2018-12-03 15:36:29 +0100

    added delete url to the job route

--- routes/jobs.js
+++ routes/jobs.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var jobControllers = require('../controllers/jobControllers')
 router.post('/create', jobControllers.createJob);
+router.get('/delete/:id', jobControllers.deleteJob);
+
 
 
 module.exports = router;
