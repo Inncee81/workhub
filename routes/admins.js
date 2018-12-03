379366d46a1db7a911e93f72174ffff468603dac
buka4chocksy <buka4chocksy@gmail.com>
2018-12-03 15:35:08 +0100

    removed delete job route from admin routes

--- routes/admins.js
+++ routes/admins.js
@@ -4,6 +4,5 @@ var router = express.Router();
 var adminController = require('../controllers/adminControllers');
 router.post('/create', adminController.CreateAdmin);
 router.post('/logins',adminController.login);
-router.get('/delete/:id', adminController.deleteJob);
 
 module.exports = router;
