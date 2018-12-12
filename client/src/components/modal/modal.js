0238450004415c236d58602fa263bb0d59df5d61
buka4chocksy <buka4chocksy@gmail.com>
2018-12-12 09:11:18 +0100

    made new changes

--- client/src/components/modal/modal.js
+++ client/src/components/modal/modal.js
@@ -3,16 +3,57 @@ import { Button, Modal } from 'reactstrap';
 import {Container , Row, Col} from 'reactstrap';
 import {  Form, FormGroup, Label, Input } from 'reactstrap';
 import './modal.css'
-
+import Axios from 'axios';
+import Validator from 'validator';
 
 class Modals extends Component{
     constructor(props) {
         super(props);
         this.state = {
-          modal: false
+          modal: false,
+          users:{
+            email:'',
+            password:''
+          },
+          userId:[]
         };
     
         this.toggle = this.toggle.bind(this);
+      }
+      handleChange = (event) =>{
+        this.setState({
+          users: {...this.state.users,[event.target.name]: event.target.value}
+        });
+      };
+
+      submit = users =>{
+        Axios.post('http://localhost:5000/users/login', users)
+        .then(res =>{
+          console.log(res);
+          if(res.status === 200){
+            alert(JSON.stringify(res.data.message));
+            if(!alert(JSON.stringify(res.data.message))){window.location.reload();}
+            sessionStorage.setItem();
+            sessionStorage.setItem();
+            sessionStorage.setItem();
+          }
+        })
+      }
+      handleSubmit = (e) =>{
+        e.preventDefault();
+        const errors = this.validate(this.state.users);
+        this.setState({loading: true});
+        this.setState({errors});
+        if(Object.keys(errors).length===0){
+          this.submit(this.state.users)
+        }
+      };
+
+      validate = (users) => {
+        const errors = {};
+        if (!Validator.isEmail(users.email)) errors.email = 'invalid Email';
+        if (!users.password) errors.password = 'cant be blank';
+        return errors;
       }
     
       toggle() {
@@ -32,14 +73,14 @@ class Modals extends Component{
                 <Row classname=''>
                   <Col md='12'>
                   <h4 className='login-text'><strong>User Login</strong></h4>
-                        <Form className='login-form'>
+                        <Form className='login-form' onSubmit={this.handleSubmit}>
         <FormGroup>
           <Label for="exampleEmail">Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="Insert Email" />
+          <Input type="email" name="email" id="exampleEmail" placeholder="Insert Email"  value={this.state.email} onChange={this.handleChange}/>
         </FormGroup>
         <FormGroup>
           <Label for="examplePassword">Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="Insert password " />
+          <Input type="password" name="password" id="examplePassword" placeholder="Insert password "  value={this.state.password} onChange={this.handleChange}/>
         </FormGroup>
     
         <FormGroup check>
@@ -48,7 +89,7 @@ class Modals extends Component{
             Remember me
           </Label>
         </FormGroup>
-        <Button>Submit</Button>
+        <Button value='submit' type='submit'>Submit</Button>
       </Form>
                 </Col>    
                 </Row>
--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,246 +1,231 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    InputGroup,
-    InputGroupAddon,
-    InputGroupButtonDropdown,
-    InputGroupDropdown,
-    Input,
-    Button,
-    Dropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    CardTitle, 
-    CardText,
-    Card
-   } from 'reactstrap';
-import './home.css';
-import {Container , Row, Col} from 'reactstrap';
-import NavBar from '../../components/navbar/navbar';
-import Footer from '../../components/footer/footer';
-import Axios from 'axios'
-class Home extends Component{
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButtonDropdown,
+  InputGroupDropdown,
+  Input,
+  Button,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  CardTitle,
+  CardText,
+  Card
+} from "reactstrap";
+import "./home.css";
+import { Container, Row, Col } from "reactstrap";
+import NavBar from "../../components/navbar/navbar";
+import Footer from "../../components/footer/footer";
+import Axios from "axios";
+import SubscribeModal from "../../components/subscribeModal/subscribeModal";
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    // state = {jobs:[]}
 
-    constructor(props) {
-        super(props);
-        // state = {jobs:[]}
-        
-    
-        this.toggleDropDown = this.toggleDropDown.bind(this);
-        // this.toggleSplit = this.toggleSplit.bind(this);
-        this.state = {
-          dropdownOpen: false,
-          jobs: []
-        //   splitButtonOpen: false
-        };
-      }
-      componentDidMount(){
-          Axios.get('http://localhost:5000/jobs/')
-          .then(res => {
-              console.log(res)
-            this.setState({
-                jobs: res.data.message
-            })
-          })
-          
-          
-      }
-    
-      toggleDropDown() {
+    this.toggleDropDown = this.toggleDropDown.bind(this);
+    // this.toggleSplit = this.toggleSplit.bind(this);
+    this.state = {
+      dropdownOpen: false,
+      jobs: [],
+      state: [],
+      users:[]
+      //   splitButtonOpen: false
+    };
+  }
+  componentDidMount() {
+    this.getJob();
+    this.getState();
+  }
+
+  getJob = () => {
+    Axios.get("http://localhost:5000/jobs/").then(res => {
+        console.log(res)
+      this.setState({
+        jobs: res.data.message
+      });
+    });
+  }
+
+  getState = () => {
+    Axios.get("http://localhost:5000/state/").then(res => {
+        console.log("from backend", res.data.message);
         this.setState({
-          dropdownOpen: !this.state.dropdownOpen
+          state: res.data.message
         });
-      }
-    
-    //   toggleSplit() {
-    //     this.setState({
-    //       splitButtonOpen: !this.state.splitButtonOpen
-    //     });
-    //   }
+      });
+  }
 
-      
-    render(){
-        let {jobs} = this.state;
-        console.log(this.state.jobs)
-        return(
-           <Container fluid={true}>
-               <NavBar/>
-              
-               <Row id='home-top'>
-               <Col id='home-top-part'>
-               <Row id='home-top-row'>
-                   <Col></Col>
-                   <Col><h1>Search For Jobs</h1></Col>
-                   <Col></Col>
-               </Row>
-               <Row id='home-mid-row'>
-                    <Col md='2'></Col>
-                   <Col md='4'>
-                        <InputGroup>
-                            {/* <InputGroupAddon addonType="prepend">@</InputGroupAddon> */}
-                            <Input placeholder="search for jobs" />
-                        </InputGroup>
-                   </Col>
-                    <Col md='4'>
-                        <InputGroup>
-                            <Input />
-                                <InputGroupButtonDropdown addonType="append" isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown}>
-                                <DropdownToggle caret color='success'>
-                                    Places
-                                </DropdownToggle>
-                                <DropdownMenu id='home-place-dropdown'>
-                                    <DropdownItem>Abia</DropdownItem>
-                                    <DropdownItem>Adamawa</DropdownItem>
-                                    <DropdownItem>Akwa Ibom</DropdownItem>
-                                    <DropdownItem>Anambra</DropdownItem>
-                                    <DropdownItem>Bauchi</DropdownItem>
-                                    <DropdownItem>Bayelsa</DropdownItem>
-                                    <DropdownItem>Benue</DropdownItem>
-                                    <DropdownItem>Borno</DropdownItem>
-                                    <DropdownItem>Cross River</DropdownItem>
-                                    <DropdownItem>Delta</DropdownItem>
-                                    <DropdownItem>Ebonyi</DropdownItem>
-                                    <DropdownItem>Enugu</DropdownItem>
-                                    <DropdownItem>Edo</DropdownItem>
-                                    <DropdownItem>Ekiti</DropdownItem>
-                                    <DropdownItem>Gombe</DropdownItem>
-                                    <DropdownItem>Imo</DropdownItem>
-                                    <DropdownItem>Jigawa</DropdownItem>
-                                    <DropdownItem>Kaduna</DropdownItem>
-                                    <DropdownItem>Kano</DropdownItem>
-                                    <DropdownItem>Katsina</DropdownItem>
-                                    <DropdownItem>Kebbi</DropdownItem>
-                                    <DropdownItem>Kogi</DropdownItem>
-                                    <DropdownItem>Kwara</DropdownItem>
-                                    <DropdownItem>Lagos</DropdownItem>
-                                    <DropdownItem>Nasarawa</DropdownItem>
-                                    <DropdownItem>Niger</DropdownItem>
-                                    <DropdownItem>Ogun</DropdownItem>
-                                    <DropdownItem>Ondo</DropdownItem>
-                                    <DropdownItem>Osun</DropdownItem>
-                                    <DropdownItem>Oyo</DropdownItem>
-                                    <DropdownItem>Plateau</DropdownItem>
-                                    <DropdownItem>Rivers</DropdownItem>
-                                    <DropdownItem>Sokoto</DropdownItem>
-                                    <DropdownItem>Taraba</DropdownItem>
-                                    <DropdownItem>Yobe</DropdownItem>
-                                    <DropdownItem>Zamfara</DropdownItem>
+  toggleDropDown() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
 
-                                </DropdownMenu>
-                                </InputGroupButtonDropdown>
-                                <Button id='home-top-btn' color='success'>Go</Button>
-                        </InputGroup>
-                    </Col>
-                    <Col md='2'></Col>
-               </Row>
-               </Col>
-               </Row>
-               <Container>
-               <Row className='home-row'>
-               <Col md='12' id='home-latest-jobs-h3Col'><h3>Latest Jobs</h3></Col>
-               </Row>
-               <Row id='home-latest-jobs-cards'>
-                   {
-                       jobs &&
-                       jobs.length > 0 ?
-                        (
-                            jobs.map(job => {
-                                return (
-                                    <Col md='3' className='home-latest-jobs' key={job}>
-                                        <Card body>
-                                            <CardTitle>{job.jobTitle}</CardTitle>
-                                            <CardText>{job.state}</CardText>
-                                            <Button color='success'>Apply Now</Button>
-                                        </Card>
-                                    </Col>
-                                )
-                            })
-                        )
-                        :
-                        (<div>loading...</div>)
-                   }
-               </Row>
-                   {/* <Row id='home-bottom-div'> */}
-               
-                   {/* </Row> */}
-            
-               <Row className='home-row' id='home-row-sub-div'>
-                   <Col md='2'></Col>
-                   <Col md='8' id='home-subscription-div'>
-                        <Row>
-                        <Col md='7' id='home-sud-txt-cover'>
-                            <h3>Subscribe for latest job update</h3>
-                            <Row><Button id='home-subscribe-btn' color='success'>Subscribe</Button></Row>
-                        </Col>
-                        <Col md='4'></Col>
-                        </Row>
-                   </Col>
-                   <Col md='2'></Col>
-               </Row>
-               <Row >
-                   <Col md='12' id='home-bottom-header'>
-                        <h2>Browse jobs</h2>
-                   </Col>
-               </Row>
-               <Row id='home-row-bottom'>
-                   <Col md='3'>
-                        <ul>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                        </ul>
-                   </Col>
-                   <Col md='3'>
-                        <ul>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                        </ul>
-                   </Col>
-                   <Col md='3'>
-                        <ul>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                        </ul>
-                   </Col>
-                   <Col md='3'>
-                        <ul>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                            <li>Porn Vancy</li>
-                        </ul>
-                   </Col>
-               </Row>
-               </Container>
-               <Footer/>
-           </Container>
-        )
+    toggleSplit() {
+      this.setState({
+        splitButtonOpen: !this.state.splitButtonOpen
+      });
     }
+
+  render() {
+    let { jobs } = this.state;
+    console.log(this.state.jobs)
+    const { state } = this.state;
+
+    console.log(state);
+    return (
+      <Container fluid={true}>
+        <NavBar />
+
+        <Row id="home-top">
+          <Col id="home-top-part">
+            <Row id="home-top-row">
+              <Col />
+              <Col>
+                <h1>Search For Jobs</h1>
+              </Col>
+              <Col />
+            </Row>
+            <Row id="home-mid-row">
+              <Col md="2" />
+              <Col md="4">
+                <InputGroup>
+                  {/* <InputGroupAddon addonType="prepend">@</InputGroupAddon> */}
+                  <Input placeholder="search for jobs" />
+                </InputGroup>
+              </Col>
+              <Col md="4">
+                <InputGroup>
+                  <Input />
+                  <InputGroupButtonDropdown
+                    addonType="append"
+                    isOpen={this.state.dropdownOpen}
+                    toggle={this.toggleDropDown}
+                  >
+                    <DropdownToggle caret color="success">
+                      Places
+                    </DropdownToggle>
+                    <DropdownMenu id="home-place-dropdown">
+                      {/*  */}
+                      {state &&
+                        state.map((sta, i) => {
+                            console.log(sta.state)
+                          return (
+                            <DropdownItem key={i}>{sta.state}</DropdownItem>
+                          );
+                        })}
+                    </DropdownMenu>
+                  </InputGroupButtonDropdown>
+                  <Button id="home-top-btn" color="success">
+                    Go
+                  </Button>
+                </InputGroup>
+              </Col>
+              <Col md="2" />
+            </Row>
+          </Col>
+        </Row>
+        <Container>
+          <Row className="home-row">
+            <Col md="12" id="home-latest-jobs-h3Col">
+              <h3>Latest Jobs</h3>
+            </Col>
+          </Row>
+          <Row id="home-latest-jobs-cards">
+            {jobs && jobs.length > 0 ? (
+              jobs.map(job => {
+                return (
+                  <Col md="3" className="home-latest-jobs" key={job}>
+                    <Card body>
+                      <CardTitle>{job.jobTitle}</CardTitle>
+                      <CardText>{job.state}</CardText>
+                      <Button color="success">Apply Now</Button>
+                    </Card>
+                  </Col>
+                );
+              })
+            ) : (
+              <div>loading...</div>
+            )}
+          </Row>
+          {/* <Row id='home-bottom-div'> */}
+
+          {/* </Row> */}
+
+          <Row className="home-row" id="home-row-sub-div">
+            <Col md="2" />
+            <Col md="8" id="home-subscription-div">
+              <Row>
+                <Col md="7" id="home-sud-txt-cover">
+                  <h3>Subscribe for latest job update</h3>
+                  
+                      <SubscribeModal subscribe='subscribe'/>
+                  
+                </Col>
+                <Col md="5"></Col>
+              </Row>
+            </Col>
+            <Col md="2" />
+          </Row>
+          <Row>
+            <Col md="12" id="home-bottom-header">
+              <h2>Browse jobs</h2>
+            </Col>
+          </Row>
+          <Row id="home-row-bottom">
+            <Col md="3">
+              <ul>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+              </ul>
+            </Col>
+            <Col md="3">
+              <ul>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+              </ul>
+            </Col>
+            <Col md="3">
+              <ul>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+              </ul>
+            </Col>
+            <Col md="3">
+              <ul>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+                <li>Porn Vancy</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+        <Footer />
+      </Container>
+    );
+  }
 }
 
-
-
-
-
-
-
-
-
-export default Home
+export default Home;
